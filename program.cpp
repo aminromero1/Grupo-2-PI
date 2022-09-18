7ff4b8d3eba7fdce79bf1cdb7fe11cd41b911632
aminromero3 <111714536+aminromero1@users.noreply.github.com>
2022-09-17 21:04:36 -0300

    Arreglamos: Sintaxis y Error "multiCharacter"

--- program.cpp
+++ program.cpp
@@ -3,99 +3,133 @@
 using namespace std;
 
 int main(){
-int opcionprincipal;//variable que almacena el numero de la opción del menu principal
-char opcioncase1;
-char opcioncase2;
-char opcioncase3;
-const int cantclientes=10;//dimensión fisica
-int Codigo=cantclientes;//la cantidad de código de prestatario es la misma que la cantidad de prestatarios
-char Categorias;
-string NombreyApellido;
-char Prestamo;
-char Prestatario[cantclientes]; //sacar de main y   poner dentro de una función asi no ocupa espacio
-    do{
-        cout<<"1.Administrar y consultar Categorías y Prestatarios"<<endl;
-        cout<<"2. Administrar Préstamos"<<endl;
-        cout<<"3. Consultar Préstamos"<<endl;
-        cout<<"ingrese una opcion: ";
-        cin>>opcionprincipal;
-        switch (opcionprincipal)
-        {
-            case 1: do{
-                        cout<<"Administrar y consultar Categorías y Prestatarios"<<endl;
-                        cout<<"  i) Agregar categoría"<<endl;
-                        cout<<"  ii) Modificar categoría"<<endl;
-                        cout<<"  iii) Eliminar categoría"<<endl;
-                        cout<<"  iv) Agregar prestatario"<<endl;
-                        cout<<"  v) Modificar prestatario"<<endl;
-                        cout<<"  vi) Eliminar prestatario"<<endl;
-                        cout<<"ingrese la opcion: ";
-                        cin>>opcioncase1;
-                        switch (tolower((opcioncase1))){
-                                case 'i':cout<<"usted eligio la op 1"<<endl; //sacar los cout y poner las funciones
-                                    break;
-                                case 'ii':cout<<"usted eligio la op 2"<<endl;
-                                    break;
-                                case 'iii':cout<<"usted eligio la op 3"<<endl;
-                                    break;
-                                case 'iv':cout<<"usted eligio la op 4"<<endl;
-                                    break;
-                                case 'v':cout<<"usted eligio la op 5"<<endl;
-                                    break;
-                                case 'vi':cout<<"usted eligio la op 6";
-                                 break;
-                        
-                                default:cout<<"ELIJA UNA OPCION CORRECTA"<<endl;
-                                    break;}
+    int opcionPrincipal;//variable que almacena el numero de la opción del menu principal
+    char opcionCase1;
+    char opcionCase2;
+    char opcionCase3;
+    const int cantClientes=10;//dimensión fisica
+    int codigo=cantClientes;//la cantidad de código de prestatario es la misma que la cantidad de prestatarios
+    char categorias;
+    string nombreyApellido;
+    char prestamo;
+    char prestatario[cantClientes]; //sacar de main y   poner dentro de una función asi no ocupa espacio
+        do{
+            cout << "1. Administrar y consultar Categorías y Prestatarios" << endl;
+            cout << "2. Administrar Préstamos" << endl;
+            cout << "3. Consultar Préstamos" << endl;
+            cout << "ingrese una opcion: ";
+            cin >> opcionPrincipal;
 
-                    }while(opcioncase1!='i' , opcioncase1!='ii', opcioncase1!='iii', opcioncase1!='iv',opcioncase1!='v',opcioncase1!='vi' );
-                break;
-            case 2: do{
-                        cout<<"Administrar Préstamos"<<endl; //AGREAGAR SWITCH ctrl c + ctrl v
-                        cout<<"  i) Agregar préstamo"<<endl;
-                        cout<<"  ii) Modificar préstamo"<<endl;
-                        cout<<"  iii) Eliminar préstamo"<<endl;
-                        cout<<"  iv) Devolver préstamo"<<endl;
-                    switch (tolower((opcioncase2))){
-                            case 'i':cout<<"usted eligio la op 1"<<endl; //sacar los cout y poner las funciones
-                                break;
-                            case 'ii':cout<<"encontraste el secreto"<<endl;
-                                break;
-                            case 'iii':cout<<"usted eligio la op 3"<<endl;
-                             break;
-                            case 'iv':cout<<"usted eligio la op 4"<<endl;
-                             break;
-                
-                            default:cout<<"ELIJA UNA OPCION CORRECTA"<<endl; 
-                            break;}
+            switch (opcionPrincipal)
+            {
+            case 1:
+                do
+                {
+                    cout << "Administrar y consultar Categorías y Prestatarios" << endl;
+                    cout << "  A) Agregar categoría" << endl;
+                    cout << "  B) Modificar categoría" << endl;
+                    cout << "  C) Eliminar categoría" << endl;
+                    cout << "  D) Agregar prestatario" << endl;
+                    cout << "  E) Modificar prestatario" << endl;
+                    cout << "  F) Eliminar prestatario" << endl;
+                    cout << "ingrese la opcion: ";
+                    cin >> opcionCase1;
+                    switch (tolower((opcionCase1)))
+                    {
+                    case 'a':
+                        cout << "usted eligio la op 1" << endl; // sacar los cout y poner las funciones
+                        break;
+                    case 'b':
+                        cout << "usted eligio la op 2" << endl;
+                        break;
+                    case 'c':
+                        cout << "usted eligio la op 3" << endl;
+                        break;
+                    case 'd':
+                        cout << "usted eligio la op 4" << endl;
+                        break;
+                    case 'e':
+                        cout << "usted eligio la op 5" << endl;
+                        break;
+                    case 'f':
+                        cout << "usted eligio la op 6";
+                        break;
 
-                    }while(opcioncase2!='i' , opcioncase2!='ii', opcioncase2!='iii', opcioncase2!='iv');
-                break;
-            case 3: do{
-                        cout<<"Consultar Préstamos"<<endl; //AGREAGAR SWITCH ctrl c + ctrl v
-                        cout<<"  i) Cantidad de objetos prestados por categoría"<<endl;
-                        cout<<"  ii) Listado de préstamos por categoría"<<endl;
-                        cout<<"  iii) Listado de préstamos ordenados por categoría o prestatario"<<endl;
-                        cout<<"  iv) Listar todos los prestatarios que tienen al menos un objeto prestado"<<endl;
-                        switch (tolower((opcioncase3))){
-                            case 'i':cout<<"usted eligio la op 1"<<endl; //sacar los cout y poner las funciones
-                                break;
-                            case 'ii':cout<<"usted eligio la op 2"<<endl;
-                                break;
-                            case 'iii':cout<<"usted eligio la op 3"<<endl;
-                             break;
-                            case 'iv':cout<<"usted eligio la op 4"<<endl;
-                             break;
-                
-                            default:cout<<"ELIJA UNA OPCION CORRECTA"<<endl;
-                            break;}
+                    default:
+                        cout << "ELIJA UNA OPCION CORRECTA" << endl;
+                        break;
+                    }
 
-                    }while(opcioncase3!='i' , opcioncase3!='ii', opcioncase3!='iii', opcioncase3!='iv');
+                } while (opcionCase1 != 'a', opcionCase1 != 'b', opcionCase1 != 'c', opcionCase1 != 'd', opcionCase1 != 'e', opcionCase1 != 'f');
                 break;
-            default:cout<<"POR FAVOR INGRESE UNA OPCION VALIDA"<<endl;
-                break;
-        }}while(opcionprincipal<1 or opcionprincipal>3); 
+            case 2:
+                do
+                {
+                    cout << "  Administrar Préstamos" << endl; // AGREAGAR SWITCH ctrl c + ctrl v
+                    cout << "  A) Agregar préstamo" << endl;
+                    cout << "  B) Modificar préstamo" << endl;
+                    cout << "  C) Eliminar préstamo" << endl;
+                    cout << "  D) Devolver préstamo" << endl;
+                    cout << "ingrese la opcion: ";
+                    cin >> opcionCase2;
+                    switch (tolower((opcionCase2)))
+                    {
+                    case 'a':
+                        cout << "usted eligio la op 1" << endl; // sacar los cout y poner las funciones
+                        break;
+                    case 'b':
+                        cout << "encontraste el secreto" << endl;
+                        break;
+                    case 'c':
+                        cout << "usted eligio la op 3" << endl;
+                        break;
+                    case 'd':
+                        cout << "usted eligio la op 4" << endl;
+                        break;
 
-return 0;
+                    default:
+                        cout << "ELIJA UNA OPCION CORRECTA" << endl;
+                        break;
+                    }
+
+                } while (opcionCase2 != 'a', opcionCase2 != 'b', opcionCase2 != 'c', opcionCase2 != 'd');
+                break;
+            case 3:
+                do
+                {
+                    cout << "Consultar Préstamos" << endl;
+                    cout << "  A) Cantidad de objetos prestados por categoría" << endl;
+                    cout << "  B) Listado de préstamos por categoría" << endl;
+                    cout << "  C) Listado de préstamos ordenados por categoría o prestatario" << endl;
+                    cout << "  D) Listar todos los prestatarios que tienen al menos un objeto prestado" << endl;
+                    cout << "ingrese la opcion: ";
+                    cin >> opcionCase3;
+                    switch (tolower((opcionCase3)))
+                    {
+                    case 'a':
+                        cout << "usted eligio la op 1" << endl; // sacar los cout y poner las funciones
+                        break;
+                    case 'b':
+                        cout << "usted eligio la op 2" << endl;
+                        break;
+                    case 'c':
+                        cout << "usted eligio la op 3" << endl;
+                        break;
+                    case 'd':
+                        cout << "usted eligio la op 4" << endl;
+                        break;
+
+                    default:
+                        cout << "ELIJA UNA OPCION CORRECTA" << endl;
+                        break;
+                    }
+                } while (opcionCase3 != 'a', opcionCase3 != 'b', opcionCase3 != 'c', opcionCase3 != 'd');
+                    break;
+            default:
+                cout << "POR FAVOR INGRESE UNA OPCION VALIDA" << endl;
+                break;
+            }
+        } while (opcionPrincipal != 1, opcionPrincipal != 2, opcionPrincipal != 3);
+        
+    return 0;
 }
-//CORREGIR LOS CASE "vi" XQ TOMAN EL ULTIMO DIGITO
