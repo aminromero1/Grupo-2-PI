c19cd4268dce0a0a4c8c36834df9716f4d9424bc
aminromero3 <111714536+aminromero1@users.noreply.github.com>
2022-09-20 00:32:06 -0300

    Agregamos: funciones y llamados a funciones

--- program.cpp
+++ program.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include<string>
 using namespace std;
+
+// Opcion 1
 void agregarCategoria(char Categoria){}//utilizar un cin para pedir el nombre de la categoria y guardar
+
 char categoriaPrincipal[];//array categoria principal
+
 char modificarCategoria(char categoriaPrincipal []){}/*usar cin y for para modificar */
 
+char eliminarCategoria(char categoriaPrincipal []){}/*usar cin y for para eliminar */
+
+const int cantClientes=10;
+char agregarPrestatario(char prestatario[cantClientes]){} /*usar cin y for para agregar */
+
+char modificarPrestatario(char prestatario[cantClientes]){} /*usar cin y for para modificar */
+
+char eliminarPrestatario(char prestatario[cantClientes]){} /*usar cin y for para eliminar */
+
+// Opcion 2
+
+char agregarPrestamo(){}//utilizar un cin para pedir el nombre de la categoria y guardar
+
+char modificarPrestamo(){}//utilizar un cin para pedir el nombre de la categoria y guardar
+
+char eliminarPrestamo(){}//utilizar un cin para pedir el nombre de la categoria y eliminar
+
+char devolverPrestamo(){}//utilizar un cin para pedir el nombre de la categoria y guardar
+
+// Opcion 3
+
+char cantObjetosPorCategoria(){}// utilizar un cin para pedir el nombre de la categoria y mostrar la cantidad de objetos
+
+char listadoPrestamosPorCategoria(){}// utilizar un cin para pedir el nombre de la categoria y mostrar los prestamos
+
+char listadoPrestamosPorPrestatario(){}//   utilizar un cin para pedir el nombre del prestatario y mostrar los prestamos
+
+char prestatariosConobjetosPrestados(){}// mostrar los prestatarios que tienen objetos prestados
+
+//***********************
+
+// funcion principal
 int main(){
     int opcionPrincipal;//variable que almacena el numero de la opción del menu principal
     char opcionCase1;
@@ -41,28 +77,32 @@ int main(){
                     {
                     case 'a':
                         cout << "usted eligio la op 1" << endl; // sacar los cout y poner las funciones
+                        agregarCategoria(categorias);
                         break;
                     case 'b':
                         cout << "usted eligio la op 2" << endl;
+                        modificarCategoria(categoriaPrincipal);
                         break;
                     case 'c':
                         cout << "usted eligio la op 3" << endl;
+                        eliminarCategoria(categoriaPrincipal);
                         break;
                     case 'd':
                         cout << "usted eligio la op 4" << endl;
+                        agregarPrestatario(prestatario);
                         break;
                     case 'e':
                         cout << "usted eligio la op 5" << endl;
+                        modificarPrestatario(prestatario);
                         break;
                     case 'f':
-                        cout << "usted eligio la op 6";
+                        cout << "usted eligio la op 6" << endl;
+                        eliminarPrestatario(prestatario);
                         break;
-
                     default:
                         cout << "ELIJA UNA OPCION CORRECTA" << endl;
                         break;
                     }
-
                 } while (opcionCase1 != 'a', opcionCase1 != 'b', opcionCase1 != 'c', opcionCase1 != 'd', opcionCase1 != 'e', opcionCase1 != 'f');
                 break;
             case 2:
@@ -78,18 +118,21 @@ int main(){
                     switch (tolower((opcionCase2)))
                     {
                     case 'a':
-                        cout << "usted eligio la op 1" << endl; // sacar los cout y poner las funciones
+                        cout << "usted eligio la op 1" << endl;
+                        agregarPrestamo();
                         break;
                     case 'b':
                         cout << "encontraste el secreto" << endl;
+                        modificarPrestamo();
                         break;
                     case 'c':
                         cout << "usted eligio la op 3" << endl;
+                        eliminarPrestamo();
                         break;
                     case 'd':
                         cout << "usted eligio la op 4" << endl;
+                        devolverPrestamo();
                         break;
-
                     default:
                         cout << "ELIJA UNA OPCION CORRECTA" << endl;
                         break;
@@ -111,17 +154,20 @@ int main(){
                     {
                     case 'a':
                         cout << "usted eligio la op 1" << endl; // sacar los cout y poner las funciones
+                        cantObjetosPorCategoria();
                         break;
                     case 'b':
                         cout << "usted eligio la op 2" << endl;
+                        listadoPrestamosPorCategoria();
                         break;
                     case 'c':
                         cout << "usted eligio la op 3" << endl;
+                        listadoPrestamosPorPrestatario();
                         break;
                     case 'd':
                         cout << "usted eligio la op 4" << endl;
+                        prestatariosConobjetosPrestados();
                         break;
-
                     default:
                         cout << "ELIJA UNA OPCION CORRECTA" << endl;
                         break;
